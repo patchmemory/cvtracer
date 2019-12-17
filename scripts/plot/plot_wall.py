6270a8fded8afaf5ba180cd62cac9277bf5f08e4
adam patch <ae.patch@gmail.com>
2019-12-17 12:59:08 -0500

    rearranged scripts directory

--- scripts/plot/plot_wall.py
+++ scripts/plot/plot_wall.py
@@ -8,6 +8,32 @@ from Analysis.Archive import Archive
 import matplotlib.cm as mpl_cm
 import matplotlib.colors as colors
 from mpl_toolkits.axes_grid1 import ImageGrid
+import scipy.stats as sps
+
+def area_circle(r):
+  return np.pi*r**2
+
+def area_shell(r1,r2):
+  return area_circle(r2) - area_circle(r1)
+
+def next_radius(r0,area_shell):
+  return np.sqrt( area_shell/np.pi + r0**2 )
+
+def area_shell_from_bins(r, nbins = 10):
+  return area_circle(r)/nbins
+
+def bin_edges(r, nbins = 10):
+    a_shell = area_shell_from_bins(r, nbins = nbins)
+    edges = [ 0 ] 
+    for i in range(nbins):
+      edges.append(next_radius(edges[-1],a_shell))
+    return np.array(edges)
+
+def print_bins(edges):
+    print("# bin  edge0  edge1   area ")
+    for i in range(1,len(edges)):
+        print("  %3i %6.2f %6.2f %6.2f "  % (i,edges[i-1],edges[i],area_shell(edges[i-1],edges[i])))
+
 
 def aspect_ratio(x_range,y_range):
     xlen = x_range[1] - x_range[0]
@@ -47,6 +73,7 @@ def combine_all_trials_in_set(t,n,tag):
         except:
             print("  Trial data not accessible... ")
             trial.print_info()
+
     return np.array(dm)
 
 
@@ -84,6 +111,89 @@ def plot_set(d_dm, t, n, tag, save = False):
     plt.tight_layout()
     if save:
         plt.savefig("results/%s_%02i_dw_thetaw_%s.png" % (t,n,tag))
+    else:
+        plt.show()
+    plt.clf()
+
+def dhist_dx(binc,mean,err):
+    _binc = ( binc[1:] + binc[:-1] ) / 2 
+    dx = binc[1:] - binc[:-1]
+    dmean = mean[1:] - mean[:-1]
+    derr = np.sqrt(err[1:]**2 + err[:-1]**2)
+    return _binc, dmean/dx, derr
+
+def figure_wall_distance_mean_orientation( d_dm, t, ns, tag, d_bins = 100, m_bins = 100, 
+                            d_range = [0, 55.5], m_range = [0,np.pi], save = False, bins = 10,  
+                            orientation = True, dhistdx = False):
+
+    plt.rcParams.update({'font.size': 18})
+
+    plt.xlabel("Distance (cm)")
+    if dhistdx:
+        plt.title("%s wall turns" % t)
+        if orientation:
+            plt.ylabel(r"Derivative of Orientation ($d\theta_{i,w}/dx$)")
+        else:
+            plt.ylabel(r"Derivative of Alignment ($d\cos2\theta_{i,w}/dx$)")
+    else:
+        plt.title("%s wall coordinates" % t)
+        if orientation:
+            plt.ylabel(r"Mean Orientation ($\theta_{i,w}$)")
+        else:
+            plt.ylabel(r"Mean Alignment ($\cos2\theta_{i,w}$)")
+
+    edges = bin_edges(d_range[1], nbins = bins)
+    print_bins(edges)
+    edges = -edges + d_range[1]
+    print_bins(edges)
+    edges = np.flipud(edges)
+    edges = np.abs(edges)
+    print_bins(edges)
+
+    binc = ( edges[1:] + edges[:-1] ) / 2
+    if dhistdx:
+        flat = [ 0 for ibin in binc]
+    else:
+        if orientation:
+            flat = [ np.pi/2 for ibin in binc]
+        else:
+            flat = [ 0 for ibin in binc]
+    plt.plot(binc, flat, 'k')
+
+    
+    for j in range(len(ns)):
+        k = set_key(t,ns[j])
+
+        print("Binning %s..." %k) 
+        count, edge = np.histogram(d_dm[k][:,0], bins=edges, range=[0,50], weights=None)
+        if orientation:
+            mean, edge, binn = sps.binned_statistic(d_dm[k][:,0],d_dm[k][:,1],statistic='mean',bins=edges,range=[0,50])
+            stdd, edge, binn = sps.binned_statistic(d_dm[k][:,0],d_dm[k][:,1],statistic='std',bins=edges,range=[0,50])
+        else:
+            mean, edge, binn = sps.binned_statistic(d_dm[k][:,0],np.cos(2*d_dm[k][:,1]),statistic='mean',bins=edges,range=[0,50])
+            stdd, edge, binn = sps.binned_statistic(d_dm[k][:,0],np.cos(2*d_dm[k][:,1]),statistic='std',bins=edges,range=[0,50])
+
+        ax_label = "n = %i" % (ns[j])
+        if dhistdx:
+            dbin, dhdx, derr = dhist_dx(binc,mean,stdd/np.sqrt(count-1))
+            plt.errorbar(dbin, dhdx, derr, label=ax_label)
+            
+        else:
+            plt.errorbar(binc,mean, stdd/np.sqrt(count-1),label=ax_label)
+
+    plt.legend()
+    plt.tight_layout()
+    if save:
+        if dhistdx:
+            if orientation:
+                plt.savefig("results/%s_dw_dcos2thetawdx_mean_nbin%02i_%s.png" % (t,bins,tag))
+            else:
+                plt.savefig("results/%s_dw_dthetawdx_mean_nbin%02i_%s.png" % (t,bins,tag))
+        else:
+            if orientation:
+                plt.savefig("results/%s_dw_cos2thetaw_mean_nbin%02i_%s.png" % (t,bins,tag))
+            else:
+                plt.savefig("results/%s_dw_thetaw_mean_nbin%02i_%s.png" % (t,bins,tag))
     else:
         plt.show()
     plt.clf()
@@ -159,6 +269,7 @@ def figure_wall_distance_orientation(d_dm, ts, ns, tag, d_bins = 100, m_bins = 1
         plt.savefig("results/figure_dw_thetaw_%s.png" % (tag))
     else:
         plt.show()
+    plt.clf()
 
 
 
@@ -172,8 +283,7 @@ tag = "t10to30_o0.0_v001.0to100.0_w-25.0to025.0_nbf3"
 #tag = "t10to30_o0.0_v000.0to100.0_w-25.0to025.0_nbf3"
 fname = "analysis_%s.arc" % tag
 arc.load(fname)
-calc=True
-
+calc=False
 
 ts = [ "SF", "Pa", "Ti", "Mo" ]
 #ts = [ "SF", "Pa" ]
@@ -181,7 +291,7 @@ ns = [ 1, 2, 5, 10 ]
 
 import pickle
 cwf = "wall_interaction.pik"
-if os.path.isfile(cwf):
+if os.path.isfile(cwf) and not calc:
     with open(cwf, 'rb') as handle:
         d_dm = pickle.load(handle)
 else:
@@ -198,5 +308,20 @@ for key in d_dm:
 
 plot_all_sets(d_dm,ts,ns,tag) 
 
-figure_wall_distance_orientation(d_dm, ts, ns, tag, save = True)
-print("figure printed")
+nbins= [5,10,20]
+#figure_wall_distance_orientation(d_dm, ts, ns, tag, save = True)
+dhistdxs = [False, True]
+orientations = [False, True]
+for dhistdx in dhistdxs:
+    for orientation in orientations:
+        for nbin in nbins: 
+            for t in ts:
+                figure_wall_distance_mean_orientation(d_dm, t, ns, tag, bins = nbin, save = True, orientation = orientation, dhistdx = dhistdx)
+            print("figure printed")
+
+
+
+
+
+
+
